a0829cce612b24d1f0e13f74b2c0c560722b2b3f
Andy Pérez <andypr97vlc@gmail.com>
2025-09-29 14:41:26 +0200

    Completado primer ejercicio LS

---
+++ Tema1/EjerciciosTema/Monaco2017_schedule/src/main/java/com/ad/ieseljust/monaco2017_schedule/Monaco2017_schedule.java
@@ -0,0 +1,68 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+package com.ad.ieseljust.monaco2017_schedule;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
+
+/**
+ *
+ * @author jasb
+ */
+public class Monaco2017_schedule {
+
+    public Document OpenXML(String name) throws IOException, SAXException, ParserConfigurationException, FileNotFoundException {
+
+        // Create an instance of DocumentBuilderFactory
+        DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+        // Using the DocumentBuilderFactory instance we create a DocumentBuilder
+        DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+        //And we use DocumentBuilder's "parse" method to get the document
+        Document doc = dBuilder.parse(new File(name));
+
+        return doc;
+    }
+
+    public void carregaXML(String filename) {
+        
+        try {
+            
+            Document root = OpenXML(filename);
+            
+            NodeList resultatsPilots = root.getElementsByTagName("Result");
+             
+             //Recorrer els resultats dels pilots i crear instancies de ResultadoCarrera. Posteriorment mostrar el contingut amb el mètode toString de ResultadoCarrera(). 
+             
+                                                
+            
+        } catch (IOException | SAXException | ParserConfigurationException ex) {
+            System.out.println("Error:" + ex.getMessage());
+        }
+
+    }
+
+    public static void main(String[] args) {
+        //TO-DO 
+        // Crear l'app a partir de la classe i crida a carregarXML amb el nom de l'arxiu .xml que retornarà
+        // (funció ArrayList<ResultadoCarrera> carregaResultadosXML(String nomXML))
+        //      - Carregar en memoria el .xml  (openXML)
+        //      - Obtindre el NodeList del "Results", recorrer-los i crear un objecte ResultadoCarrera passant-li l'item (Element) del NodeList. 
+        //      - afegir-lo a una ArrayList ArrayList<ResultadoCarrera> elsResultats = new ArrayList() que retornarem;
+
+        Monaco2017_schedule app = new Monaco2017_schedule();
+
+        app.carregaXML("monaco_2017.xml");
+
+    }
+}
---
+++ Tema1/EjerciciosTema/ls.java
@@ -0,0 +1,143 @@
+package Tema1.EjerciciosTema;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class ls {
+    public static void main(String[] args) {
+        // Función principal: gestiona los argumentos y llama a la función correspondiente
+        if (args.length != 2) {
+            System.out.println("Número de argumentos incorrecto.\n\nSintáxis: \n\t java ls [ lista | columnas | tabla ] ruta de la carpeta");
+            System.exit(0);
+        }
+
+        try {
+            if (args[0].equals("lista")) {
+                ls.mostrarLista(args[1]);
+            } else if (args[0].equals("columnas")) {
+                ls.mostrarColumnas(args[1]);
+            } else if (args[0].equals("tabla")) {
+                ls.mostrarTabla(args[1]);
+            } else {
+                System.out.println("No entiendo la orden " + args[0] + "\n");
+            }
+        } catch (Exception ex) {
+            System.out.println("Error: " + ex.getMessage());
+            Logger.getLogger(ls.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    // Muestra los archivos y carpetas de un directorio en formato de lista simple
+    private static void mostrarLista(String ruta) {
+        File f = new File(ruta);
+
+        if (f.exists()) {
+            if (f.isDirectory()) {
+                String[] archivos = f.list();
+                System.out.println("El directorio \"" + ruta + "\" contiene: ");
+                for (String archivo : archivos) {
+                    System.out.println("\t- " + archivo);
+                }
+            } else {
+                System.out.println("La ruta especificada no es un directorio.");
+            }
+        } else {
+            System.out.println("La ruta especificada no existe.");
+        }
+    }
+
+    // Muestra los archivos y carpetas en varias columnas
+    private static void mostrarColumnas(String ruta) {
+        File f = new File(ruta);
+
+        if (f.exists()) {
+            if (f.isDirectory()) {
+                String[] archivos = f.list();
+                ls.columnas(archivos);
+            } else {
+                System.out.println("La ruta especificada no es un directorio.");
+            }
+        } else {
+            System.out.println("La ruta especificada no existe.");
+        }
+    }
+
+    // Da formato en columnas a los nombres de archivo recibidos (Código del profesor)
+    private static void columnas(String[] filenames) {
+        int MAX_FILES_BY_COLUMN = 4;
+
+        int columnas = (filenames.length / MAX_FILES_BY_COLUMN) + 1;
+
+        String[][] salida = new String[MAX_FILES_BY_COLUMN][columnas];
+
+        // Llenar la matriz 'salida' con los nombres de archivo
+        for (int i = 0; i < filenames.length; i++) {
+            salida[i % MAX_FILES_BY_COLUMN][i / MAX_FILES_BY_COLUMN] = filenames[i];
+        }
+        // Encontrar el nombre de archivo más largo para establecer el ancho de columna
+        int maxLength = 0;
+        for (String filename : filenames) {
+            if (filename.length() > maxLength) {
+                maxLength = filename.length();
+            }
+        }
+        // Ajustar el formato para que cada columna tenga el mismo ancho
+        String format = "%-" + (maxLength + 2) + "s";
+        // Bucle para mostrar salida con formato simétrico
+        for (int i = 0; i < MAX_FILES_BY_COLUMN; i++) {
+            for (int j = 0; j < columnas; j++) {
+                if (salida[i][j] != null) {
+                    System.out.printf(format, salida[i][j]); // printf para aplicar el formato
+                }
+            }
+            System.out.println();
+        }
+    }
+
+    // Muestra los archivos y carpetas en formato de tabla
+    private static void mostrarTabla(String ruta) {
+        File f = new File(ruta);
+
+        if (f.exists()) {
+            if (f.isDirectory()) {
+                File[] archivos = f.listFiles();
+
+                int tamañoNombre = 0;
+                for (File archivo : archivos) {
+                    if (archivo.getName().length() > tamañoNombre) {
+                        tamañoNombre = archivo.getName().length();
+                    }
+                }
+
+                String formato = "%-" + (tamañoNombre + 2) + "s | %10s | %s%n";
+
+                System.out.printf(formato, "Nombre", "Tamaño", "Información");
+
+                for (File archivo : archivos) {
+                    ArrayList<String> info = new ArrayList<>();
+
+                    if (archivo.isDirectory()) info.add("D");
+                    else info.add("F");
+
+                    if (archivo.canRead()) info.add("R");
+                    else info.add("-");
+
+                    if (archivo.canWrite()) info.add("W");
+                    else info.add("-");
+                        
+                    if (archivo.isHidden()) info.add("H");
+                    else info.add("-");
+
+                    System.out.printf(formato, archivo.getName(), archivo.length(), String.join("", info));
+                }
+            } else {
+                System.out.println("La ruta especificada no es un directorio.");
+            }
+        } else {
+            System.out.println("La ruta especificada no existe.");
+        }
+    }
+
+}
